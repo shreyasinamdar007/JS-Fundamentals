a593c6b4d8c3418af939c7cd6e41c0d01dc56fad
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2023-11-11 12:44:26 +0530

    Iterations, arrays, and objects

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+// if
+const isLoggedIn = true;
+const temperature = 42;
+
+if (temperature < 50) {
+  console.log("less than 50");
+}
+
+console.log("temperature is greater than 50");
+
+const balance = 1000;
+
+if (balance > 500) console.log("You are rich"), console.log("test2");
